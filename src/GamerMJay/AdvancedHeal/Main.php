8d560b941cd074a4de05c197a225c48f11f20cc8
GamerMJay <66413999+GamerMJay@users.noreply.github.com>
2022-02-20 01:13:09 +0100

    Update Main.php
    Added Main.php

--- src/GamerMJay/AdvancedHeal/Main.php
+++ src/GamerMJay/AdvancedHeal/Main.php
@@ -1,1 +1,26 @@
-comming soon...
+<?php
+
+namespace GamerMJay\BetterGameMode;
+
+#pocketmine
+use GamerMJay\AdvancedHeal\commands\heal;
+use GamerMJay\AdvancedHeal\commands\feed;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Tile;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+class Main extends PluginBase {
+    public $config;
+
+    public function onEnable(): void {
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml", 2);
+        $this->getServer()->getCommandMap()->register("gm0", new gm0($this));
+        $this->getServer()->getCommandMap()->register("gm1", new gm1($this));
+    }
+}
